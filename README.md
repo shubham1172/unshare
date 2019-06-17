de68b5fef411d45e245818192604c5e5a3540de9
Shubham Sharma <shubhamsharma1172@gmail.com>
2019-06-17 23:51:27 +0530

    Update docs and build config

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 Python extension for C's unshare call. <br>
 See [man 2 unshare](http://man7.org/linux/man-pages/man2/unshare.2.html).
 
+# Installing
+
+```bash
+$ pip3 install unshare
+```
 
 # Usage
 
--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, Extension
 
+with open('README.md', 'r') as f:
+    longdesc = f.read()
+
 unshare_module = Extension('unshare', ['unshare.c'])
 
 setup(
     name = 'unshare', 
-    version = 0.1, 
+    version = 0.21, 
     author = 'Shubham Sharma',
     author_email = 'shubhamsharma1172@gmail.com',
     description = 'Python extension for C\'s unshare call (man 2 unshare)',
+    long_description = longdesc,
+    long_description_content_type = 'text/markdown',
     url = 'https://github.com/shubham1172/unshare',
     license = 'GPLv3',
     platforms = 'Linux',
