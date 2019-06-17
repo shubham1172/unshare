4dfcae201a2d7ef45595dac7f84f826e5fc07687
Shubham Sharma <shubhamsharma1172@gmail.com>
2019-06-18 00:22:29 +0530

    reindent

--- unshare.c
+++ unshare.c
@@ -1,27 +1,27 @@
 #include<Python.h>
 
 static PyObject *_unshare(PyObject *self, PyObject *args) {
-	int clone_flags;
+    int clone_flags;
 
-	if (!PyArg_ParseTuple(args, "i", &clone_flags))
-		return NULL;
+    if (!PyArg_ParseTuple(args, "i", &clone_flags))
+        return NULL;
 
-	if (unshare(clone_flags) == -1) {
-		PyErr_SetFromErrno(PyExc_RuntimeError);
-		return NULL;
-	} else {
-		Py_INCREF(Py_None);
-		return Py_None;
-	}
+    if (unshare(clone_flags) == -1) {
+        PyErr_SetFromErrno(PyExc_RuntimeError);
+        return NULL;
+    } else {
+        Py_INCREF(Py_None);
+        return Py_None;
+    }
 }
 
 static PyMethodDef unshare_methods[] = {
-    {"unshare", _unshare, METH_VARARGS, 
-	"Use as: unshare(flag)\n"
-	"possible flags: CLONE_NEWNS, CLONE_NEWUTS, CLONE_NEWPID, "
-	"CLONE_NEWUSER, CLONE_NEWIPC, CLONE_NEWNET, CLONE_THREAD\n"
-	"See man 2 unshare.\n"
-	},
+    {"unshare", _unshare, METH_VARARGS,
+    "Use as: unshare(flag)\n"
+    "possible flags: CLONE_NEWNS, CLONE_NEWUTS, CLONE_NEWPID, "
+    "CLONE_NEWUSER, CLONE_NEWIPC, CLONE_NEWNET, CLONE_THREAD\n"
+    "See man 2 unshare.\n"
+    },
     {NULL, NULL, 0, NULL}
 };
 
@@ -37,13 +37,13 @@ PyMODINIT_FUNC PyInit_unshare(void) {
     PyObject *module = PyModule_Create(&unshare_module);
 
     // clone constants
-	PyModule_AddIntConstant(module, "CLONE_NEWNS", CLONE_NEWNS);     // mount namespace
-	PyModule_AddIntConstant(module, "CLONE_NEWUTS", CLONE_NEWUTS);   // UTS (hostname) namespace
-	PyModule_AddIntConstant(module, "CLONE_NEWPID", CLONE_NEWPID);   // PID namespace
-	PyModule_AddIntConstant(module, "CLONE_NEWUSER", CLONE_NEWUSER); // users namespace
-	PyModule_AddIntConstant(module, "CLONE_NEWIPC", CLONE_NEWIPC);   // IPC namespace
-	PyModule_AddIntConstant(module, "CLONE_NEWNET", CLONE_NEWNET);   // network namespace
-	PyModule_AddIntConstant(module, "CLONE_THREAD", CLONE_THREAD);
+    PyModule_AddIntConstant(module, "CLONE_NEWNS", CLONE_NEWNS);     // mount namespace
+    PyModule_AddIntConstant(module, "CLONE_NEWUTS", CLONE_NEWUTS);   // UTS (hostname) namespace
+    PyModule_AddIntConstant(module, "CLONE_NEWPID", CLONE_NEWPID);   // PID namespace
+    PyModule_AddIntConstant(module, "CLONE_NEWUSER", CLONE_NEWUSER); // users namespace
+    PyModule_AddIntConstant(module, "CLONE_NEWIPC", CLONE_NEWIPC);   // IPC namespace
+    PyModule_AddIntConstant(module, "CLONE_NEWNET", CLONE_NEWNET);   // network namespace
+    PyModule_AddIntConstant(module, "CLONE_THREAD", CLONE_THREAD);
 
     return module;
 }
