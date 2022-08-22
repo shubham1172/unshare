dc143eb4bc221309478ad8f71e5e3d1b41ea5cf3
Avery Whitaker <averywhitaker@gmail.com>
2022-08-22 18:59:12 -0500

    add more flags

--- unshare.c
+++ unshare.c
@@ -38,13 +38,20 @@ PyMODINIT_FUNC PyInit_unshare(void) {
     PyObject *module = PyModule_Create(&unshare_module);
 
     // clone constants
-    PyModule_AddIntConstant(module, "CLONE_NEWNS", CLONE_NEWNS);     // mount namespace
-    PyModule_AddIntConstant(module, "CLONE_NEWUTS", CLONE_NEWUTS);   // UTS (hostname) namespace
-    PyModule_AddIntConstant(module, "CLONE_NEWPID", CLONE_NEWPID);   // PID namespace
-    PyModule_AddIntConstant(module, "CLONE_NEWUSER", CLONE_NEWUSER); // users namespace
-    PyModule_AddIntConstant(module, "CLONE_NEWIPC", CLONE_NEWIPC);   // IPC namespace
-    PyModule_AddIntConstant(module, "CLONE_NEWNET", CLONE_NEWNET);   // network namespace
-    PyModule_AddIntConstant(module, "CLONE_THREAD", CLONE_THREAD);
+    PyModule_AddIntConstant(module, "CLONE_NEWNS", CLONE_NEWNS);         // mount namespace
+    PyModule_AddIntConstant(module, "CLONE_NEWUTS", CLONE_NEWUTS);       // UTS (hostname) namespace
+    PyModule_AddIntConstant(module, "CLONE_NEWPID", CLONE_NEWPID);       // PID namespace
+    PyModule_AddIntConstant(module, "CLONE_NEWUSER", CLONE_NEWUSER);     // users namespace
+    PyModule_AddIntConstant(module, "CLONE_NEWIPC", CLONE_NEWIPC);       // IPC namespace
+    PyModule_AddIntConstant(module, "CLONE_NEWNET", CLONE_NEWNET);       // network namespace
+    PyModule_AddIntConstant(module, "CLONE_FILES", CLONE_FILES);         // file descriptor table
+    PyModule_AddIntConstant(module, "CLONE_FS", CLONE_FS);               // filesystem attributes
+    PyModule_AddIntConstant(module, "CLONE_NEWCGROUP", CLONE_NEWCGROUP); // cgroup namespace
+    PyModule_AddIntConstant(module, "CLONE_NEWTIME", CLONE_NEWTIME);     // time namespace
+    PyModule_AddIntConstant(module, "CLONE_SYSVSEM", CLONE_SYSVSEM);     // System V semaphore adjustment values
+    PyModule_AddIntConstant(module, "CLONE_THREAD", CLONE_THREAD);       // thread group
+    PyModule_AddIntConstant(module, "CLONE_THREAD", CLONE_SIGHAND);      // signal handlers and blocked signals
+    PyModule_AddIntConstant(module, "CLONE_THREAD", CLONE_VM);           // virtual memory
 
     return module;
 }
